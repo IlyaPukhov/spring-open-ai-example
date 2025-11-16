824727b77ac0abfc66b260204cf1a502e3fef1c2
Ilya Pukhov <pukhovin@answer-42.ru>
2025-11-16 10:22:26 +0300

    Initial commit

---
+++ src/main/java/com/puhovin/claudespringai/ClaudeSpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.puhovin.claudespringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeSpringAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaudeSpringAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puhovin/claudespringai/ClaudeSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puhovin.claudespringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeSpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
